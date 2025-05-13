76866c0e1d60fcac225be4f04200f392c394f1ad
glaplantwd <glaplantwd@gmail.com>
2025-05-13 18:13:09 -0500

    dungeons view

--- public/assets/js/views/js/dungeonsView.js
+++ public/assets/js/views/js/dungeonsView.js
@@ -1,5 +1,8 @@
 import { fullwidthHeroSection } from "/zeldaMVC/public/assets/js/views/shared/hero.js";
 
+
+dungeonsView();
+
 function dungeonsView() {
 	fullwidthHeroSection("dungeons_zelda_one.webp");
 }
