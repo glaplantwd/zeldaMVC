5a85eada4a2beedb83a3340f307fef4ebdc9dede
glaplantwd <glaplantwd@gmail.com>
2025-05-14 09:14:17 -0500

    characters hero image

--- public/assets/js/views/js/charactersView.js
+++ public/assets/js/views/js/charactersView.js
@@ -1,7 +1,7 @@
 import { fullwidthHeroSection } from "/zeldaMVC/public/assets/js/views/shared/hero.js";
 
 function charactersView() {
-	fullwidthHeroSection("zelda-concept-art-7.jpg");
+	fullwidthHeroSection("characters.webp");
 }
 
 export { charactersView };
