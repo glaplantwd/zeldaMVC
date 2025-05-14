db749f2133dd76b4c491003dac78a523439912c3
glaplantwd <glaplantwd@gmail.com>
2025-05-13 19:11:02 -0500

    hero section

--- public/assets/js/views/shared/hero.js
+++ public/assets/js/views/shared/hero.js
@@ -1,5 +1,7 @@
 function fullwidthHeroSection(imgSrc) {
 
+	if(!imgSrc) return;
+	
 	const heroSection = document.querySelector(".full-width-hero-section");
 	const heroImage = document.createElement("img");
 	
@@ -7,18 +9,24 @@ function fullwidthHeroSection(imgSrc) {
 	heroImage.classList.add("hero-image");
 	heroImage.alt = "Legend of Zelda Link Fan Art Concept Illustration";
 	
-	if(!heroSection){
+	if(heroSection){
+		heroSection.innerhtml = '';
+	}
+	else{
 		
-	const fullwidthHeroSection = document.createElement("section");
+		const fullwidthHeroSection = document.createElement("section");
+		fullwidthHeroSection.classList.add("full-width-hero-section");
+		document.querySelector("#main-content").appendChild(fullwidthHeroSection);
+	}
+
 	const fullwidthHero = document.createElement("div");
 	
-	fullwidthHeroSection.classList.add("full-width-hero-section");
 	fullwidthHero.classList.add("row", "full-width-hero");
 	fullwidthHero.appendChild(heroImage);	
 	fullwidthHeroSection.appendChild(fullwidthHero);
+
 	
-	document.querySelector("#main-content").appendChild(fullwidthHeroSection);
-}
+
 }
 
 
