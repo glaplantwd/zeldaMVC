39a30cf00863fb9d32575dd96fd891072de26a01
glaplantwd <glaplantwd@gmail.com>
2025-05-13 19:02:39 -0500

    hero image

--- public/assets/js/views/shared/hero.js
+++ public/assets/js/views/shared/hero.js
@@ -1,27 +1,22 @@
-function fullwidthHeroSection($imgSrc) {
+function fullwidthHeroSection(imgSrc) {
 
 	const heroSection = document.querySelector(".full-width-hero-section");
 	const heroImage = document.createElement("img");
 	
-	if(heroSection){
-		
-		heroImage.src = `https://glaplantwd.github.io/zeldaMVC/public/assets/images/heroImages/${$imgSrc}`;
+	heroImage.src = `https://glaplantwd.github.io/zeldaMVC/public/assets/images/heroImages/${imgSrc}`;
+	heroImage.classList.add("hero-image");
+	heroImage.alt = "Legend of Zelda Link Fan Art Concept Illustration";
 	
-	}
-	else{
+	if(!heroSection){
+		
 	const fullwidthHeroSection = document.createElement("section");
 	const fullwidthHero = document.createElement("div");
 	
-	
-
 	fullwidthHeroSection.classList.add("full-width-hero-section");
 	fullwidthHero.classList.add("row", "full-width-hero");
-	heroImage.classList.add("hero-image");
-
-	fullwidthHero.appendChild(heroImage);
-	heroImage.src = `https://glaplantwd.github.io/zeldaMVC/public/assets/images/heroImages/${$imgSrc}`;
-	heroImage.alt = "Legend of Zelda Link Fan Art Concept Illustration";	
+	fullwidthHero.appendChild(heroImage);	
 	fullwidthHeroSection.appendChild(fullwidthHero);
+	
 	document.querySelector("#main-content").appendChild(fullwidthHeroSection);
 }
 }
