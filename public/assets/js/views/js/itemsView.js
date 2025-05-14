248efdce53f3e73bfbd2ba41b05800f4274a94db
glaplantwd <glaplantwd@gmail.com>
2025-05-14 09:23:51 -0500

    items hero

--- public/assets/js/views/js/itemsView.js
+++ public/assets/js/views/js/itemsView.js
@@ -1,6 +1,6 @@
 import { fullwidthHeroSection } from "/zeldaMVC/public/assets/js/views/shared/hero.js";
 function itemsView() {
-	fullwidthHeroSection("zelda_misc.png");
+	fullwidthHeroSection("retro_items.webp");
 }
 
 export { itemsView };
